6fc45386288dc0a7cc9c670d4ea293ef7f682b2e
KwonEunchan <klg3377@naver.com>
2022-07-29 14:17:38 +0900

    header update and menu create

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Menu from './components/Menu';
 import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
        <Header></Header>
-       <Login></Login>
+       <Menu></Menu>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,11 @@
 import '../styles/Header.scss'
+import logo from '../images/logo_text.png';
 
 export default function Header(){
   return(
     <>
       <header>
-        <div className="header-top">
-          <p>로그인이 필요합니다</p>
-        </div>
-        <div className="header-bottom">
-          <input type="text" placeholder='검색어를 입력하세요'/>
-        </div>
+        <img src={logo} alt="Logo"></img>
       </header>
     </>
   )
