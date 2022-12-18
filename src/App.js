19c1bf9d2225241a4affb5a4ef314c258a49592a
KwonEunchan <klg3377@naver.com>
2022-12-18 23:36:38 +0900

    123

--- src/App.js
+++ src/App.js
@@ -6,33 +6,12 @@ import logo from './images/logo.png'
 function App() {
   const [rotateCount,setRotateCount] = useState(0)
   const [randomValue,setRandomValue] = useState(Math.floor(Math.random()*list.list.length))
-  const [flag,setFlag] = useState('Normal')
   useEffect(()=>{
     document.querySelector('.cardFront').style.transform = `rotateY(${180*rotateCount}deg)`
     document.querySelector('.cardBack').style.transform = `rotateY(${180*(rotateCount-1)}deg)`
     let temp = randomValue
     while(randomValue===temp){
       temp = Math.floor(Math.random()*list.list.length)
-      if(flag==="Cheap"){
-        console.log(temp)
-        while(list.list[randomValue].price < list.list[temp].price){
-          if(list.list[randomValue].price===0 ) break
-          temp = Math.floor(Math.random()*list.list.length)
-        }
-        setFlag("Normal")
-        
-      }
-      else if(flag==="Close"){
-        if(list.list[randomValue].loc.substring(0) > list.list[temp].loc.substring(0)){
-          temp = randomValue
-        }
-        else{
-          setFlag("Normal");
-        }
-      }
-      else{
-        temp = Math.floor(Math.random()*list.list.length)
-      }
     }
     setTimeout(() => {
       setRandomValue(temp)
@@ -46,7 +25,7 @@ function App() {
             
           }}>
             <div className='logoBox'>
-              <img className='logo' src={logo}></img>
+              <img className='logo' alt="logo" src={logo}></img>
             </div>
             <div className='infoBox'>
               <p className='nameText'>{ list.list[randomValue].name }</p>
@@ -57,7 +36,7 @@ function App() {
           </div>
           <div className='cardBack'>
           <div className='logoBox'>
-              <img className='logo' src={logo}></img>
+              <img className='logo' alt="logo" src={logo}></img>
             </div>
             <div className='infoBox'>
               <p className='nameText'>{ list.list[randomValue].name }</p>
@@ -70,15 +49,6 @@ function App() {
         <div className='btnReplay' onClick={(e)=>{
           setRotateCount(rotateCount+1)
         }}>다른 거 먹어요!</div>
-        <div className='btnCheap' onClick={(e)=>{
-          setFlag("Cheap")
-          setRotateCount(rotateCount+1)
-        }}>조금 비싸요</div>
-        <div className='btnClose' onClick={(e)=>{
-          setRotateCount(rotateCount+1)
-          setFlag("Close")
-        }}>조금 멀어요</div>
-        
       </div>
       
     </div>
